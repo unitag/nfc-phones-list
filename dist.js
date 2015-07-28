52ad22f869e1f79730c542cb397b81384ce3f5d2
Simon <tws.contact@gmail.com>
2015-07-28 15:04:38 +0200

    Dist.js - export and minify json files

---
+++ dist.js
@@ -0,0 +1,41 @@
+'use strict';
+
+var fs = require('fs'),
+	globby = require('globby'),
+	Q = require('q');
+
+var SRC_PATH = './data/*.json',
+	DEST_PATH = './dist/nfcList';
+
+var nfcList = [];
+
+function readJson(path) {
+	return Q.nfcall(fs.readFile, path, 'utf-8').then(function (json) {
+		json = JSON.parse(json);
+		nfcList.push(json[0]);
+	});
+}
+
+function writeJson() {
+	fs.writeFile(DEST_PATH + '.json', JSON.stringify(nfcList, null, 4), function(err) {
+		if(err) {
+			console.log(err);
+		} else {
+			console.log('JSON saved to ' + DEST_PATH + '.json');
+		}
+	});
+	fs.writeFile(DEST_PATH + '.min.json', JSON.stringify(nfcList), function(err) {
+		if(err) {
+			console.log(err);
+		} else {
+			console.log('JSON saved to ' + DEST_PATH + '.min.json');
+		}
+	});
+}
+
+var promise = Q.nfcall(globby, SRC_PATH)
+	.invoke('map', readJson).all()
+	.then(writeJson);
+
+promise.done();
+
